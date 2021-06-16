c2f74b1d347cc52f7fca73a2c86b220bfe919195
Rifka Annisa <Geminigirl124@gmail.com>
2021-06-16 11:42:43 +0700

    halaman tidak ditemukan

--- app.js
+++ app.js
@@ -15,22 +15,31 @@ app.set('views', 'views');
 
 const adminRoutes = require('./routes/admin');
 
+const errorController = require('./controllers/error');
+
 app.use(bodyPaser.urlencoded({extended: true}));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/admin',adminRoutes.routes);
 
-db.execute('SELECT * FROM PENGGUNA')
-  .then(result => {
-    console.log(result);
-  }).catch(err => {
-    console.log(err);
-  });
+
+// db.execute('SELECT * FROM PENGGUNA')
+//   .then(result => {
+//     console.log(result);
+//   }).catch(err => {
+//     console.log(err);
+//   });
 
 //
 // app.get('/', (req, res) => {
 //   console.log("hello");
 //   res.send('<h1>hello</h1>')
 // });
+
+app.get('/', (req, res) => {
+  res.render('login.ejs');
+});
+
+app.use(errorController.get404);
 
 app.listen(3001);

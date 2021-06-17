133169988fd620358f99b874f97c0d0b1f457846
Rifka Annisa <Geminigirl124@gmail.com>
2021-06-17 16:01:16 +0700

    menggunakan sequelize

--- controllers/admin.js
+++ controllers/admin.js
@@ -9,18 +9,17 @@ exports.getAdminDashboard = (req,res) => {
   });
 };
 
-// exports.getDataPengguna = (req,res, next) => {
-//   Pengguna.fetchAll()
-//   .then(([rows, fieldData]) => {
-//     res.render('admin', {
-//       users: rows,
-//       coloms: fieldData,
-//       pageTitle: 'Pengguna',
-//       path: '/pengguna'
-//     });
-//   })
-//   .catch(err => console.log(err));
-// };
+exports.getDataPengguna = (req,res, next) => {
+  Pengguna.findAll()
+  .then(pengguna => {
+    res.render('admin', {
+      users: pengguna,
+      pageTitle: 'Pengguna',
+      path: '/pengguna'
+    });
+  })
+  .catch(err => console.log(err));
+};
 
 exports.postAddDataPengguna = ( req,res, next) => {
   const nik = req.body.nik;
--- routes/admin.js
+++ routes/admin.js
@@ -9,7 +9,7 @@ const router = express.Router();
 const adminController = require('../controllers/admin');
 
 router.get('/' , adminController.getAdminDashboard );
-// router.get('/pengguna' , adminController.getDataPengguna );
+router.get('/pengguna' , adminController.getDataPengguna );
 router.post('/pengguna' , adminController.postAddDataPengguna );
 // router.get('/pengguna/:penggunaId' , adminController.getDetailDataPengguna );
 

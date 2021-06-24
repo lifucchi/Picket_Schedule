a5e0ee35c1ec5cb07f663df9d5282da11c765f10
Rifka Annisa <Geminigirl124@gmail.com>
2021-06-24 20:17:00 +0700

    +pengguna untuk menambah

--- controllers/meja.js
+++ controllers/meja.js
@@ -2,13 +2,20 @@ const Meja = require('../models/meja');
 const Pengguna = require('../models/pengguna');
 
 exports.getDataMeja = (req,res, next) => {
-  Meja.findAll( {include: Pengguna} )
-  .then( table => {
-    res.render('./admin/checklistmeja', {
-      tables: table,
-      pageTitle: 'Checklist Meja',
-      path: '/checklistmeja'
-    });
+
+  Pengguna.findAll()
+  .then(pengguna => {
+
+    Meja.findAll( {include: Pengguna} )
+    .then( table => {
+      res.render('./admin/checklistmeja', {
+        tables: table,
+        users: pengguna,
+        pageTitle: 'Checklist Meja',
+        path: '/checklistmeja'
+      });
+    })
+
   })
   .catch(err => console.log(err));
 };
